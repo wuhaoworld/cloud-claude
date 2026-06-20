0c1b4158a089efeb45ae07cc1a7ace86d09fa88d
hiratwu <ratwu@qq.com>
2026-06-20 10:57:17 +0800

    feat: enable email and password authentication in auth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,9 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 
 export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true
+    },
     database: drizzleAdapter(db, {
         provider: "sqlite",
     }),
